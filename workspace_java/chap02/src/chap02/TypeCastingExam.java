c278a119da46ffb0b9dd291be5e7c106813b5ba9
holytiger9954@naver.com <>
2026-01-06 17:23:08 +0900

    01.06 java 학습내용 업데이트

---
+++ workspace_java/chap02/src/chap02/TypeCastingExam.java
@@ -0,0 +1,64 @@
+package chap02;
+
+public class TypeCastingExam {
+
+	public static void main(String[] args) {
+		
+		int intValue = 10;
+		byte byteValue = (byte)intValue;
+		System.out.println("intValue: " + intValue);
+		
+		// 여기서 ()는 우선 순위 연산자
+		int a = 2 * (3 + 4);
+		
+		// 여기서 ()는 형 변환 연산자
+		byte byteValue2 = (byte)intValue;
+		
+		intValue = 200;
+		byteValue = (byte)intValue;
+		
+		// 정확히는 비트를 2의 보수로 계산한 결과
+		// 즉 예상하기 힘들다
+		System.out.println("byteValue: " + byteValue);
+		
+		double d1 = 3.14;
+		float f1 = (float)d1;
+		System.out.println("d1: " + d1);
+		
+//		long l1 = (long)2200000000;
+		
+		double d2 = -3.14;
+		int i1 = (int)d2;
+		System.out.println("i1: " + i1);
+		
+		// 작은 것에서 큰 것으로 변환할 때
+		// 형 변환 생략가능
+		int i2 = 100;
+		long l2 = (long)i2;
+		long l3 = i2;
+		long l4 = 100;
+		
+		int i3 = 2100000000;
+		int i4 = 2100000000;
+		int i5 = i3 + i4;
+		System.out.println("i5: " + i5); // i5 = -94967296
+		
+		int i6 = 10;
+		long l6 = 4l;
+//		int i7 = i6 + l6;
+		long l7 = i6 + l6;
+		// int는 long으로 자동 형 변환이 된다
+		// int + long 연산 시 int가 long이 되고 결과도 long이 된다
+		
+		int i7 = 10;
+		double d7 = 5.5;
+		double d8 = i7 + d7; // 15.5
+		System.out.println("d8:[15.5?] " + d8);
+		
+		double d9 = 10 / 4;
+		System.out.println("d9: " + d9);
+		double d10 = 10.0 / 4;
+		System.out.println("d10: " + d10);
+	}
+
+}
--- workspace_java/chap02/src/chap02/VarTypeExam.java
+++ workspace_java/chap02/src/chap02/VarTypeExam.java
@@ -30,7 +30,128 @@ public class VarTypeExam {
 		System.out.println("s2: " + s2);
 		String s3 = "\\허\t성\n범";
 		System.out.println("s3: " + s3);
+		
+		System.out.println("글씨" + 3);
+		System.out.println("글씨" + 3 + 2); // 글씨32
+		System.out.println("글씨" + (3 + 2)); // 글씨5
+		System.out.println(3 + 2 + "글씨"); // 5글씨
+		
+//		int a = 3.14;
+		
+		float f1 = 3.14f;
+		System.out.println("f1: " + f1);
+		
+		double d1 = 3.14;
+		System.out.println("d1: " + d1);
+		
+		float f2 = 0.1234567890123f;
+		System.out.println(f2);
+		// float의 정밀도: 소수점 7번째 자리까지
+
+		double d2 = 0.12345678901234567890;
+		System.out.println("d2: " + d2);
+		// double의 정밀도: 소수점 16번째 자리까지
+		
+		double d3 = 5e3;
+		System.out.println("d3: " + d3);
+		
+		boolean stop = true;
+		boolean start = false;
+		
+		System.out.println("stop: " + stop);
+		
+		// 문제1
+		// 내 나이를 저장
+		int age = 28;
+		System.out.println("내 나이는 " + age +"살");
+		
+		// 문제2
+		// 운전 면허가 있다/없다
+		boolean license = true;
+		System.out.println("내 운전면허: " + license);
+		// *문제2 확장
+		if (license = true)
+		System.out.println("나 운전면허 있어");
+		
+		// 문제3
+		// 우리 집에 있는 스마트폰의 개수
+		int phone_num = 4;
+		System.out.println("우리 집은 스마트폰 " + phone_num + "개 있다~");
+		
+		// 문제4
+		// 내 이름
+		String name = "허성범";
+		System.out.println("내 이름은 " + name);
+		
+		// 문제5
+		// 1평은 3.3 제곱미터
+		// 5평이 몇 제곱미터인지
+		double room = 3.3;
+		System.out.println("5평은 " + (room * 5) + " 제곱미터");
+		
+		// 문제6
+		// 6-1: 두 변수 x, y에 각 숫자를 넣고
+		// 출력결과: "3 > 4 결과는 false 입니다."
+		int x = 3;
+		int y = 4;
+		boolean result1 = x > y;
+		System.out.println(x + " > "+ y + " 결과는 " + result1 + "입니다.");
+		
+		boolean result2 = y > x;
+		// 6-2: x, y 값 바꿔서 정답 나오는지 출력
+		System.out.println(y + " > "+ x + " 결과는 " + result2 + "입니다.");
+		
+		// 문제7
+		/*
+		 * 숫자 149
+		 * ---------
+		 * 출력결과
+		 * ---------
+		 * "백의 자리: 1"
+		 * "십의 자리: 4"
+		 * "일의 자리: 9"
+		 */
+		int num = 149;
+		
+		int num_100 = num / 100;
+		int num_10 = num % 100 / 10;
+		int num_1 = num % 100 % 10 / 1;
+		System.out.println("백의 자리: " + num_100 +  "\n십의 자리: " + num_10 + "\n일의 자리: " + num_1);
+		
+		int n100 = num / 100;
+		int n10 = (num - n100 * 100) / 10;
+		int n1 = (num - n100 * 100 - n10 * 10) / 1;
+		System.out.println("백의 자리: " + n100 +  "\n십의 자리: " + n10 + "\n일의 자리: " + n1);
+		
+		// 문제8
+		// 회식비 43000원
+		// 참석인원 4명
+		// 인당 얼마?
+		// 8-1: 디테일하게 원단위
+		int cost = 43000;
+		int member = 4;
+		int cost1 = cost / member;
+		System.out.println("정산: " + cost1 + "원");
+		// 8-2: 백원단위까지만 받기
+		int cost2 = cost / (member * 100) * 100;
+		System.out.println("백원 단위 정산: " + cost2 + "원");
+		// 8-2-1: 주최자는 얼마? (나머지는 만원단위만)
+		int cost3 = cost / (member * 10000) * 10000;
+		System.out.println("만원 단위 정산: " + cost3 + "원");
+		int cost4 = cost - cost3 * (member - 1);
+		System.out.println("주최자: " + cost4 + "원");
+		
+		// 문제9
+		String left = "오예스";
+		String right = "사탕";
+		
+		// 뭔가 하기
+		String center = left;
+		left = right;
+		right = center;
 				
+		System.out.println("left: " + left); // 사탕
+		System.out.println("right: " + right); // 오예스
 	}
 
 }
